dc83458b963912f227291893230276177b838b98
Alex Dzyoba <avd@reduct.ru>
2015-02-25 13:07:45 +0300

    Fixed indentation for python external sorting

--- sorting/external/guido-ext-sort.py
+++ sorting/external/guido-ext-sort.py
@@ -8,12 +8,12 @@ assert array.array('i').itemsize == 4
 
 def intsfromfile(f):
 	while True:
-		 a = array.array('i')
-		 a.fromstring(f.read(4000))
-		 if not a:
-			 break
-		 for x in a:
-			 yield x
+		a = array.array('i')
+		a.fromstring(f.read(4000))
+		if not a:
+			break
+		for x in a:
+			yield x
 
 if len(sys.argv) != 3:
 	print("{} <filename> <buffer size>".format(sys.argv[0]), file=sys.stderr)
@@ -21,7 +21,9 @@ if len(sys.argv) != 3:
 
 bufsize = int(sys.argv[2])
 inf = open(sys.argv[1], "rb")
+
 iters = []
+
 start = time.clock()
 while True:
 	a = array.array('i')
@@ -41,8 +43,8 @@ a = array.array('i')
 for x in heapq.merge(*iters):
 	a.append(x)
 	if len(a) >= 1000:
-			a.tofile(sys.stdout.buffer)
-			del a[:]
+		a.tofile(sys.stdout.buffer)
+		del a[:]
 end = time.clock()
 if a:
 	a.tofile(sys.stdout.buffer)
